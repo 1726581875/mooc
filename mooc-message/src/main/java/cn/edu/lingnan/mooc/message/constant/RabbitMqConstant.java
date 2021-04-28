f4b9c776dc6a3025e3ed1fabe654d5ece615bb7d
xiaomingzhang <1726581875@qq.com>
2021-04-28 09:47:51 +0800

    feat:消息推送，在消息服务使用rabbitmq消费消息（未测试）

--- mooc-message/src/main/java/cn/edu/lingnan/mooc/message/constant/RabbitMqConstant.java
+++ mooc-message/src/main/java/cn/edu/lingnan/mooc/message/constant/RabbitMqConstant.java
@@ -2,7 +2,6 @@ package cn.edu.lingnan.mooc.message.constant;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
--- mooc-message/src/main/java/cn/edu/lingnan/mooc/message/jms/Consumer.java
+++ mooc-message/src/main/java/cn/edu/lingnan/mooc/message/jms/Consumer.java
@@ -1,7 +1,12 @@
 package cn.edu.lingnan.mooc.message.jms;
+import cn.edu.lingnan.mooc.message.model.entity.Notice;
+import cn.edu.lingnan.mooc.message.service.SendNoticeService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,6 +18,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class Consumer {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private SendNoticeService sendNoticeService;
 
     /**
      * 消息消费者，负责接收消息
@@ -21,6 +30,14 @@ public class Consumer {
     @RabbitListener(queues = "${constant.mq.messageQueueName:mooc.mq.messageQueue}")
     public void messageConsumer(String message){
         log.info("messageConsumer accept message : {}",message);
+        Notice notice = null;
+        try {
+            notice = objectMapper.readValue(message, Notice.class);
+        } catch (JsonProcessingException e) {
+            log.error("==== 转换消息发生错误 ==== message={} ",message);
+            return;
+        }
+        sendNoticeService.saveAndSendNotice(notice);
     }
 
 
