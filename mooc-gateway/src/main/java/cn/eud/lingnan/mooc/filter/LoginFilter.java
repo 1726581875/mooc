9d74d0e07006c08ea61a9c7033e8563899b49f34
v-xiaomz <1726581875@qq.com>
2022-06-07 14:16:04 +0800

    fix:处理依赖问题导致报错

--- mooc-gateway/src/main/java/cn/eud/lingnan/mooc/filter/LoginFilter.java
+++ mooc-gateway/src/main/java/cn/eud/lingnan/mooc/filter/LoginFilter.java
@@ -11,11 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
@@ -34,10 +32,9 @@ public class LoginFilter implements GlobalFilter, Ordered {
         ServerHttpResponse response = exchange.getResponse();
 
         URI requestURI = request.getURI();
-        String toString = requestURI.toString();
-
-        // TODO 获取ip地址
+        String url = requestURI.toString();
         String ipAddress = getIpAddress(request);
+        log.info("请求url={}, ip={}", url, ipAddress);
 
 
         //webflux
@@ -46,18 +43,18 @@ public class LoginFilter implements GlobalFilter, Ordered {
         String token = request.getHeaders().getFirst("Authorization");
         if(token == null) {
             return Mono.defer(() -> {
+                //response.mutate().header("Content-Type","application/json;charset=utf-8").build();
                 response.setStatusCode(HttpStatus.UNAUTHORIZED);
                 String respBody = "{\"status\":\"401\",\"msg\":\"无效token\",\"data\":null,\"success\":false}";
                 byte[] bytes = respBody.getBytes(StandardCharsets.UTF_8);
                 DataBuffer buffer = response.bufferFactory().wrap(bytes);
                 //response.getHeaders().set("permValid", "false");
-                log.info("UrlFilter拦截非法请求，没有登录检测到权限");
+                log.warn("请求不合法，没有携带token");
                 return response.writeWith(Flux.just(buffer));
             });
         }
 
         // 2、去redis验证token是否有效
-
 
 
         return chain.filter(exchange);
@@ -67,29 +64,7 @@ public class LoginFilter implements GlobalFilter, Ordered {
     public int getOrder() {
         return 0;
     }
-/*
 
-    public static String getIpAddress(HttpServletRequest request) {
-
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
-        }
-        return ip;
-    }
-*/
 
     private static String getIpAddress(ServerHttpRequest request) {
 
